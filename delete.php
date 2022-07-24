fd0e20f3fe97accb432ca73f67e454bac3411483
RubenU2002 <rsantiagourrego@gmail.com>
2022-07-23 20:04:14 -0500

    Administrador de notas 0.01

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php 
     include_once('conexion.php');
-    $cod = $_POST['codEstu'];
+    $cod = $_POST['select1'];
     
     pg_query("DELETE FROM estudiantes WHERE cod_est='$cod'");
     header("location:index.php");
--- index.php
+++ index.php
@@ -19,12 +19,39 @@
         <h1>hola</h1>
         <p>COD:</p>
         <input type="text" name="codEstu">
+        <select name="select1" id="select1" >
+            <?php 
+            $resultado=pg_query("select * from estudiantes");
+            while($row1=pg_fetch_object($resultado)){
+            ?>
+                <option> <?php  echo $row1->cod_est; ?></option>
+            <?php
+            }
+             ?>
+        </select>
         <button type="submit">borrar</button>
+        <select name="" id="">
+            <option value="">2022</option>
+            <option value="">2023</option>
+            <option value="">2024</option>
+        </select>
+
     </form>
     <form action="insertar.php" method="POST">
         <h3>Inserte su cosas</h3>
         <input type="number" name="cod_Estu">
         <input type="text" name="nomb_est">
+        
+        <select name="select" id="11" >
+            <?php 
+            $resultado=pg_query("select * from estudiantes");
+            while($row1=pg_fetch_object($resultado)){
+            ?>
+                <option value="$contador"> <?php  echo $row1->cod_est; ?></option>
+            <?php
+            }
+             ?>
+        </select>
         <button type="submit">insertarpirchurria</button>
     </form>
 </body>
